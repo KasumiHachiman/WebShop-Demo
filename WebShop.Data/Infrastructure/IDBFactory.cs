899cde9cbb94f2bfb767ff8dbe767beb885ca952
KasumiHachiman <vthuongle99@gmail.com>
2017-07-20 21:36:12 -0700

    this is work in buoi_2

---
+++ WebShop.Data/Infrastructure/IDBFactory.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebShop.Data.Infrastructure
+{
+    public interface IDBFactory : IDisposable
+    {
+        //thong qua factory gian tiep tao entity
+        WebShopDbContext Init();
+
+    }
+}
