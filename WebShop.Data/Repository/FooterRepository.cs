f3061469244b7a8c85f98672f64307b12b0eccd4
KasumiHachiman <vthuong99@gmail.com>
2017-07-25 21:45:16 +0700

    add migration and repository to database

---
+++ WebShop.Data/Repository/FooterRepository.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WebShop.Data.Infrastructure;
+using WebShop.Model.Models;
+
+namespace WebShop.Data.Repositories
+{
+    public interface IFooterRepository : IRepository<Footer>
+    {
+
+    }
+    public class FooterRepository : RepositoryBase<Footer>,IFooterRepository
+    {
+        public FooterRepository(IDBFactory dbFactory)
+            :base(dbFactory)
+        {
+
+        }
+    }
+}
---
+++ WebShop.Data/Repository/TagRepository.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WebShop.Data.Infrastructure;
+using WebShop.Model.Models;
+
+namespace WebShop.Data.Repositories
+{
+    public interface ITagRepository : IRepository<Tag>
+    {
+
+    }
+    public class TagRepository : RepositoryBase<Tag>, ITagRepository
+    {
+        public TagRepository(IDBFactory dbFactory) : base(dbFactory)
+        {
+        }
+    }
+}
