b6d7aaa02d6fdf6fb8c5a26ccf5d8cacfb2f0671
KasumiHachiman <vthuong99@gmail.com>
2017-08-08 17:40:44 +0700

    create service

---
+++ WebShop.Service/PostCategoryService.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WebShop.Data.Infrastructure;
+using WebShop.Data.Repositories;
+using WebShop.Model.Models;
+
+namespace WebShop.Service
+{
+    public interface IPostCategoryService
+    {
+        PostCategory Add(PostCategory postCategory);
+        void Update(PostCategory postCategory);
+        PostCategory Delete(string id);
+        IEnumerable<PostCategory> GetAll();
+        IEnumerable<PostCategory> GetAllByParentId(int parentId);
+        PostCategory GetById(string id);
+        void Save();
+
+    }
+    public class PostCategoryService : IPostCategoryService
+    {
+        IPostCategoryRepository _postCategoryRepository;
+        IUnitOfWork _unitOfWork;
+        public PostCategoryService(IPostCategoryRepository postCategoryRepository,IUnitOfWork unitOfWork)
+        {
+            this._postCategoryRepository = postCategoryRepository;
+            this._unitOfWork = unitOfWork;
+        }
+
+        public PostCategory Add(PostCategory postCategory)
+        {
+            return _postCategoryRepository.Add(postCategory);
+        }
+
+        public PostCategory Delete(string id)
+        {
+            return _postCategoryRepository.Delete(id);
+        }
+
+        public IEnumerable<PostCategory> GetAll()
+        {
+           return  _postCategoryRepository.GetAll();
+        }
+
+        public IEnumerable<PostCategory> GetAllByParentId(int parentId)
+        {
+            return _postCategoryRepository.GetMulti(x => x.Status && x.ParentID == parentId);
+        }
+
+        public PostCategory GetById(string id)
+        {
+            return _postCategoryRepository.GetSingleById(id);
+        }
+
+        public void Save()
+        {
+            _unitOfWork.Commit();
+        }
+
+        public void Update(PostCategory postCategory)
+        {
+            _postCategoryRepository.Update(postCategory);
+        }
+    }
+}
